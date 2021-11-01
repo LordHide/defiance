6eed759a5a5001b6f57bbbc71f731dba81aa044c
Jeremy Auvray <jeremy93-2008@hotmail.fr>
2021-11-01 03:25:10 +0100

    menu de seleccion y menu de carga

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import Profile from './Profile';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import {createIcon} from './storeManagement.js';
+//<button onClick={() => elegirpersonaje(< Profile name="trisha"/>)} > trisha </button>
+function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [isExpansion, setExpansion] = useState(false);
+  const [isCharacter, setCharacter] = useState(false);
+  const [profile, setProfile] = useState("");
 
-function App(props) {
-  const [displayContent, elegirpersonaje] = useState(<button onClick={() => elegirpersonaje(< Profile name="trisha"/>)} > trisha </button>);
-  return (
-    displayContent
-  );
+  useEffect(() => {
+    setTimeout(function(){ setLoading(false); setExpansion(true) }, 3000);
+  }, []);
+
+  return <>
+    {isLoading && <Loading/>}
+    {isExpansion && <ChooseExpansion onCharacterChange={()=>{
+      setExpansion(false);
+      setCharacter(true);
+    }} />}
+    {isCharacter && <ChoseCharacter onProfileChange={(name)=>{
+      setCharacter(false);
+      setProfile(name);
+    }}/>}
+    {profile && <Profile name={profile}/>}
+  </>
+}
+
+function Loading(){
+  return <div className="loading">
+    <div>
+      {createIcon({"type": "png", "code": "logoDefianceImg", "class": "logoLoading"})}
+      <div className="containerCircleLoading">
+        {createIcon({"type": "png", "code": "loading", "class": "circleLoading"})}
+      </div>
+    </div>
+  </div>;
+}
+
+function ChooseExpansion({onCharacterChange}){
+  return <>
+    <button onClick={() => onCharacterChange(true)} > Default </button>
+    <button onClick={() => onCharacterChange(true)} > Outcast </button>
+    <button onClick={() => onCharacterChange(true)} > Revenant </button>
+  </>
+}
+
+function ChoseCharacter({onProfileChange}){
+  return <button onClick={() => onProfileChange("trisha")} > trisha </button>
 }
 
 export default App;
--- src/pngDispenser.js
+++ src/pngDispenser.js
@@ -2,12 +2,16 @@ import trishaImg from './Img/trisha.png';
 import defaultImg from './Img/default.png';
 import miniTrishaImg from './Img/miniTrisha.png';
 import duchessImg from './Img/duchess.png';
+import logoDefianceImg from './Img/logo-defiance.png';
+import loading from './Img/loading.png';
 
 function characterImg(name){
     let imgReference;
     switch(name){
       case "trisha" : imgReference = trishaImg; break;
       case "Duchess" : imgReference = duchessImg; break;
+      case "logoDefianceImg" : imgReference = logoDefianceImg; break;
+      case "loading" : imgReference = loading; break;
       case "miniTrisha" : imgReference = miniTrishaImg; break;
       default: return imgReference = defaultImg; break;
     }
